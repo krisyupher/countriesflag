cd6122ed525c7cd9b8a590fb5825dd60e4d459a3
Cristian Florez <cflorez@quantumconnexion.com>
2020-07-28 21:51:16 -0500

    Updates

---
+++ precache-manifest.3a32a1d5b4370a0ecd7dd7af3293d6dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26bba7422493cea2da90b7a3919e88e",
+    "url": "/countriesflag/index.html"
+  },
+  {
+    "revision": "e18483e4f9807332992d",
+    "url": "/countriesflag/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "599c91be6248c03a5074",
+    "url": "/countriesflag/static/css/main.20638026.chunk.css"
+  },
+  {
+    "revision": "e18483e4f9807332992d",
+    "url": "/countriesflag/static/js/2.cb777bb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countriesflag/static/js/2.cb777bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599c91be6248c03a5074",
+    "url": "/countriesflag/static/js/main.0118a31c.chunk.js"
+  },
+  {
+    "revision": "0440308bdc4c802be0d9",
+    "url": "/countriesflag/static/js/runtime-main.5cbaa4b1.js"
+  }
+]);
